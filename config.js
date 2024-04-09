89b1da5269291895a372545a1b4a071d11a1c2e5
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2024-04-09 22:42:20 +0530

    mongoDB atlas DB setup done

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 const dbURL =
-  "mongodb+srv://bhairavnathkcww:LTn6nXPKWtO6MAy6@democluster.zzxmzyn.mongodb.net/?retryWrites=true&w=majority&appName=democluster";
+  "mongodb+srv://swapnilwalhekar1999:obicqBA7ugw4A0nG@crudcluster.phoolmw.mongodb.net/?retryWrites=true&w=majority&appName=crudCluster";
+
+// const dbURL =
+//   "mongodb+srv://bhairavnathkcww:LTn6nXPKWtO6MAy6@democluster.zzxmzyn.mongodb.net/?retryWrites=true&w=majority&appName=democluster";
 
 mongoose.connect(dbURL);
