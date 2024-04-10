6479ef6fb718901a17616ee59f1825cc9cd07eac
Swapnil Walhekar <108648658+swapnilwalhekar@users.noreply.github.com>
2024-04-10 11:54:27 +0530

    get, update and delete api completed

--- index.js
+++ index.js
@@ -14,6 +14,52 @@ app.post("/add", async (req, res) => {
   });
 });
 
+app.get("/", async (req, res) => {
+  const allProducts = await Product.find({});
+
+  res.send({
+    status: "success",
+    allProductList: allProducts,
+  });
+});
+
+app.put("/update", async (req, res) => {
+  await Product.updateOne(
+    { _id: req.query.id },
+    {
+      $set: {
+        name: req.body.name,
+        price: req.body.price,
+        brand: req.body.brand,
+        category: req.body.category,
+      },
+    }
+  );
+
+  res.send({
+    status: "success",
+    message: "Product updated successfully",
+  });
+});
+
+app.delete("/delete", async (req, res) => {
+  const selectedProduct = await Product.find({ _id: req.query.id });
+  if (selectedProduct.length === 0) {
+    res.status(404).send({
+      status: "error",
+      message: "Please provide a valid product id",
+    });
+  } else {
+    await Product.deleteOne({ _id: req.query.id });
+
+    res.send({
+      status: "success",
+      message: "Product deleted successfully",
+      deletedProduct: selectedProduct,
+    });
+  }
+});
+
 const port = 5000;
 
 app.listen(port, () => {
